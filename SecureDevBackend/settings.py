a27c8cb03cbcb862b131fac1e53e9d7ae14af851
Jelly Maweja <jellymaweja7@gmail.com>
2025-07-04 11:00:54 +0100

    ABC

--- SecureDevBackend/settings.py
+++ SecureDevBackend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',   # si tu utilises DRF pour l'API
-    'corsheaders',      # pour autoriser les requêtes depuis ton frontend React
+    'corsheaders',     
     'contact',
 ]
 
