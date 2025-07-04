e91d897efe4c02a021ae5b73d7112d44a1061ffb
Jelly Maweja <jellymaweja7@gmail.com>
2025-07-04 09:14:01 +0100

    GHL

--- render-start.sh
+++ render-start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# Migrations de base de données (optionnel)
-php bin/console doctrine:migrations:migrate --no-interaction
 
 # Démarrer le serveur PHP
-php -S 0.0.0.0:${PORT} -t public
+php -S 0.0.0.0:10000 -t public
